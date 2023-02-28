db58eef8317d008257c2f54ac9a801bf2765decd
DmitryLemeshko <126555640+DmitryLemeshko@users.noreply.github.com>
2023-02-28 13:07:38 +0200

    Add files via upload

---
+++ Завдання для спільної роботи 5.py
@@ -0,0 +1,5 @@
+dict = {'Name': 'Maxim Olegovich Komarov', 'Age': 20, 'Course': 'Accounting', 'group number': 11,} 
+print("Student Name:", dict['Name']) 
+print("Course:", dict['Course']) 
+print("Age:", dict['Age'])
+print("group number:", dict['group number'])
